d40da8d89672737256f578da8f561e5f55a4832b
rahmadhany98 <kazuha004@gmail.com>
2024-05-04 19:29:38 +0800

    basic server setup and db connection

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-console.log('Store API');
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const connectDB = require("./db/connect");
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Store API");
+});
+
+//product routes
+
+//error
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+//start the server
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
